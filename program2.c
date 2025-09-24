4031bf71fba9461584cd5b925a0fb1f26cccb77c
manmeetsingh108 <2025a1r018@mietjammu.in>
2025-09-24 13:26:21 +0530

    Add files via upload

---
+++ program2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int x,y,sum;
+    printf("Enter first value");
+    scanf("%i",&x);
+    printf("Enter second value");
+    scanf("%i",&y);
+    sum=x+y;
+    printf("Addition =%i",sum);
+    return 0;
+}
