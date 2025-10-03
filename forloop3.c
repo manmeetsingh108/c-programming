6aecb86ad69f23654a6ffcfeb714006730528e99
manmeetsingh108 <2025a1r018@mietjammu.in>
2025-10-03 15:35:49 +0530

    Create forloop3.c

---
+++ forloop3.c
@@ -0,0 +1,11 @@
+//WAP to print numbers from 1 to N
+#include<stdio.h>
+int main(){
+    int i,n;
+    printf("Enter the value of N");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        printf("%d",i);
+    }
+    return 0;
+}
