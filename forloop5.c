733b32b4dfeb07cff4614c41dc5dd58df07a2b9c
manmeetsingh108 <2025a1r018@mietjammu.in>
2025-10-03 15:47:01 +0530

    Create forloop5.c

---
+++ forloop5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int i,n;
+    printf("Enter the value of N");
+    scanf("%d",&n);
+    for(i=2; i<=n; i=i+2){
+            printf("%d",i);
+    }
+    return 0;
+}
