20ddc9c5720ecee8395d4bbe158b142cfdd85869
manmeetsingh108 <2025a1r018@mietjammu.in>
2025-10-06 10:40:30 +0530

    Create sum of digits of given number1.c

---
+++ sum of digits of given number1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int num,sum=0;
+    scanf("%d",&num);
+    while(num!=0){
+        int lastdigit = num%10;
+        sum = sum + lastdigit;
+        num = num/10;
+    }
+    printf("%d",sum);
+    return 0;
+}
