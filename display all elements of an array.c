24328d34e0f2128bfc09a1cbe8cfce3091258fab
manmeetsingh108 <2025a1r018@mietjammu.in>
2025-10-13 11:05:37 +0530

    Create display all elements of an array.c

---
+++ display all elements of an array.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int stu[5]={1,2,3,4,5};
+    //how we can display all elements of an array//
+    for(int i=0; i<5; i++){
+        printf("%d\t",stu[i]);
+    }
+    return 0;
+}
